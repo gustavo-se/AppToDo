688668ba173fd5d3e23c881156b0d9967da970b8
Gustavo <68789743+gustavo-se@users.noreply.github.com>
2021-03-12 13:00:38 -0300

    fix bug ancho de las tarjetas

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -3,20 +3,24 @@ import "../styles/ToDo.css";
 const ToDo = ({ tarea, id, handleEliminar, handleCheck, todo }) => {
   return (
     <div
-      class={`alert w-75 d-flex justify-content-between align-items-center ${
+      className={`alert container d-flex justify-content-between align-items-center ${
         todo.completed ? "alert-success" : "alert-warning"
       }`}
     >
       <div>
-        <p className={`fs-5 ${todo.completed ? "completed" : ""}`}>
-          <span>{id}</span> - {tarea}
-        </p>
+        <p className={`fs-5 ${todo.completed ? "completed" : ""}`}>{tarea}</p>
       </div>
       <div>
-        <button onClick={() => handleCheck(id)} class="btn btn-primary">
+        <button
+          onClick={() => handleCheck(id)}
+          className="btn btn-primary btn-sm"
+        >
           Check
         </button>
-        <button onClick={() => handleEliminar(id)} class="btn btn-danger mx-1">
+        <button
+          onClick={() => handleEliminar(id)}
+          className="btn btn-danger btn-sm mx-md-1"
+        >
           Eliminar
         </button>
       </div>
